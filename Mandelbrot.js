24a181adf7b385dff2ce3916dc7b72a6c68c2ad7
Pierrick Varin <pierrick.varin@zagett.com>
2014-08-04 03:04:54 +0200

    Implemented 2x super sampling

--- Mandelbrot.js
+++ Mandelbrot.js
@@ -26,10 +26,10 @@ function Mandelbrot(canvas_id) {
     this.element = document.getElementById(canvas_id);
     this.context = this.element.getContext("2d");
 
-    this.width  = this.element.width;
-    this.height = this.element.height;
+    this.width  = this.element.width * 2;
+    this.height = this.element.height * 2;
 
-    this.paletteOffset = 10;
+    this.paletteOffset = 100;
     this.coloringAlgorithm = COLORING.SMOOTH_ESCAPE_TIME; 
 
     this.fractalType = FRACTAL.MANDELBROT;
@@ -37,7 +37,7 @@ function Mandelbrot(canvas_id) {
     var _width  = 1.0 / this.width;
     var _height = 1.0 / this.height;
 
-    var imageData = this.context.createImageData(this.width, this.height);
+    var imageData = this.context.createImageData(this.width * 0.5 | 0, this.height * 0.5 | 0);
 
     this.buildDefaultPalette = function() {
         for(var p = 0; p < 512; p++) {
@@ -47,11 +47,14 @@ function Mandelbrot(canvas_id) {
     }
 
     this.getPaletteColor = function(p) {
+        if(p < 0) {
+            return [0, 0, 0];
+        }
         return this.palette[ (this.palette.length * Math.max(0, p + this.paletteOffset)) % (this.palette.length - 1) | 0 ];
     }
 
     this.canvasToFractalCoords = function(p) {
-        return this.coordsMap[p.y | 0][p.x | 0];
+        return this.coordsMap[p.y * 2 | 0][p.x * 2 | 0];
     }
 
     this.render = function(scale, px, py) {
@@ -64,8 +67,6 @@ function Mandelbrot(canvas_id) {
         if( ! this.palette.length ) {
             this.buildDefaultPalette();
         }
-
-        var imgIndex = 0;
 
         var y = py - scale_2;
 
@@ -180,20 +181,19 @@ function Mandelbrot(canvas_id) {
             y += scale_h;
         }
         
-        for (var j = 0; j < this.height; j++) {
-            for (var i = 0; i < this.width; i++) {
+        var imgIndex = 0;
 
-                if( iterationsMap[j][i] == -1 ) {
-                    var r = 0;
-                    var g = 0;
-                    var b = 0;
-                } else {
-                    var p = this.getPaletteColor(iterationsMap[j][i]);
+        for (var j = 0; j < this.height; j+=2) {
+            for (var i = 0; i < this.width; i+=2) {
 
-                    var r = p[0] | 0;
-                    var g = p[1] | 0;
-                    var b = p[2] | 0;
-                }
+                var p = this.getPaletteColor(iterationsMap[j][i]);
+                var p2 = this.getPaletteColor(iterationsMap[j][i+1]);
+                var p3 = this.getPaletteColor(iterationsMap[j+1][i]);
+                var p4 = this.getPaletteColor(iterationsMap[j+1][i+1]);
+
+                var r = ((p[0] + p2[0] + p3[0] + p4[0]) * 0.25) | 0;
+                var g = ((p[1] + p2[1] + p3[1] + p4[1]) * 0.25) | 0;
+                var b = ((p[2] + p2[2] + p3[2] + p4[2]) * 0.25) | 0;
 
                 imageData.data[imgIndex++] = r;
                 imageData.data[imgIndex++] = g;
