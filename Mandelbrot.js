308594d4ecd390817287348e99f48d579ceebb92
Pierrick Varin <pierrick.varin@zagett.com>
2014-08-08 00:46:11 +0200

    * Way simpler handling of "plugins" (No more serparation between pixel shading and the fractl iterator itself) * Added a preview mode

--- Mandelbrot.js
+++ Mandelbrot.js
@@ -15,20 +15,15 @@ function Mandelbrot(canvas_id) {
     this.element = document.getElementById(canvas_id);
     this.context = this.element.getContext("2d");
 
-    this.width  = this.element.width * 2;
-    this.height = this.element.height * 2;
+    this.width  = this.element.width*2;
+    this.height = this.element.height*2;
 
     this.paletteMultiplier = 1;
     this.paletteOffset = 220;
-    this.coloringAlgorithm = Plugins.pixelShaders.point_orbit_trap.shader; 
 
-    this.fractalFunc = Plugins.fractals.mandelbrot.iterator;
-    this.pluginParams = null;
+    this.fractalFunc = Plugins.mandelbrot_orbit_trap.iterator;
 
-    var _width  = 1.0 / this.width;
-    var _height = 1.0 / this.height;
-
-    var imageData = this.context.createImageData(this.width * 0.5 | 0, this.height * 0.5 | 0);
+    this.is_preview = false;
 
     this.buildDefaultPalette = function() {
         for(var p = 0; p < 512; p++) {
@@ -37,13 +32,8 @@ function Mandelbrot(canvas_id) {
         }
     }
 
-    this.setFractalFunction = function(funcname, params) {
-        this.pluginParams = params;
-        this.fractalFunc  = Plugins.fractals[funcname].iterator;
-    }
-
-    this.setPixelShader = function(funcname) {
-        this.coloringAlgorithm = Plugins.pixelShaders[funcname].shader;
+    this.setFractalFunction = function(func) {
+        this.fractalFunc = func;
     }
 
     this.getPaletteColor = function(p) {
@@ -55,10 +45,34 @@ function Mandelbrot(canvas_id) {
     }
 
     this.canvasToFractalCoords = function(p) {
-        return this.coordsMap[p.y * 2 | 0][p.x * 2 | 0];
+        var scale = 2;
+
+        if( this.is_preview ) {
+            scale = 1;
+        }
+
+        return this.coordsMap[p.y * scale | 0][p.x * scale | 0];
     }
 
-    this.render = function(scale, px, py) {
+    this.render = function(scale, px, py, preview) {
+        if( preview ) {
+            this.is_preview = true;
+
+            this.width  = this.element.width;
+            this.height = this.element.height;
+
+            var _width  = 1.0 / this.width;
+            var _height = 1.0 / this.height;
+
+            var imageData = this.context.createImageData(this.width | 0, this.height | 0);
+        } else {
+
+            var _width  = 1.0 / this.width;
+            var _height = 1.0 / this.height;
+            
+            var imageData = this.context.createImageData(this.width * 0.5 | 0, this.height * 0.5 | 0);
+        }
+
         var scale_2 = scale / 2;
         var scale_h = _height * scale;
         var scale_w = _width * scale;
@@ -71,7 +85,6 @@ function Mandelbrot(canvas_id) {
         }
 
         // First pass
-
         var y = py - scale_2;
 
         for (var j = 0; j < this.height; j++) {
@@ -85,15 +98,10 @@ function Mandelbrot(canvas_id) {
                 
                 this.coordsMap[j][i] = {x: x, y: y};
 
-                var converge = this.fractalFunc(x, y, this.pluginParams);
+                iterationsMap[j][i] = this.fractalFunc(x, y);
                 
-                if( converge ) {
-                    iterationsMap[j][i] = -1;
-                } else {
-                    iterationsMap[j][i] = this.coloringAlgorithm();
-                    if( iterationsMap[j][i] > maxMapValue ) {
-                        maxMapValue = iterationsMap[j][i];
-                    }
+                if( iterationsMap[j][i] > maxMapValue ) {
+                    maxMapValue = iterationsMap[j][i];
                 }
 
                 x += scale_w;
@@ -105,18 +113,29 @@ function Mandelbrot(canvas_id) {
         // Second pass. Actual rendering
         var imgIndex = 0;
         var maxValDiv = 1.0 / maxMapValue;
+        var screenIter = 2;
 
-        for (var j = 0; j < this.height; j+=2) {
-            for (var i = 0; i < this.width; i+=2) {
+        if( preview ) {
+            screenIter = 1;
+        }
+        for (var j = 0; j < this.height; j+=screenIter) {
+            for (var i = 0; i < this.width; i+=screenIter) {
 
                 var p = this.getPaletteColor(iterationsMap[j][i] * maxValDiv);
-                var p2 = this.getPaletteColor(iterationsMap[j][i+1] * maxValDiv);
-                var p3 = this.getPaletteColor(iterationsMap[j+1][i] * maxValDiv);
-                var p4 = this.getPaletteColor(iterationsMap[j+1][i+1] * maxValDiv);
+                
+                if( ! preview ) {
+                    var p2 = this.getPaletteColor(iterationsMap[j][i+1] * maxValDiv);
+                    var p3 = this.getPaletteColor(iterationsMap[j+1][i] * maxValDiv);
+                    var p4 = this.getPaletteColor(iterationsMap[j+1][i+1] * maxValDiv);
 
-                var r = ((p[0] + p2[0] + p3[0] + p4[0]) * 0.25) | 0;
-                var g = ((p[1] + p2[1] + p3[1] + p4[1]) * 0.25) | 0;
-                var b = ((p[2] + p2[2] + p3[2] + p4[2]) * 0.25) | 0;
+                    var r = ((p[0] + p2[0] + p3[0] + p4[0]) * 0.25) | 0;
+                    var g = ((p[1] + p2[1] + p3[1] + p4[1]) * 0.25) | 0;
+                    var b = ((p[2] + p2[2] + p3[2] + p4[2]) * 0.25) | 0;
+                } else {
+                    var r = p[0];
+                    var g = p[1];
+                    var b = p[2];
+                }
 
                 imageData.data[imgIndex++] = r;
                 imageData.data[imgIndex++] = g;
--- plugins.js
+++ plugins.js
@@ -1,140 +1,80 @@
-window.mandelbrot_plugin_data = {};
-
-function pass_data(dataObj) {
-	window.mandelbrot_plugin_data = dataObj;
-	return false;
-}
-
-function get_data(dataKey) {
-	if( dataKey ) {
-		return window.mandelbrot_plugin_data[dataKey];
-	} else {
-		return window.mandelbrot_plugin_data;
-	}
-}
 
 var Plugins = {
-	fractals: {
-		mandelbrot: {
-			desc: "Mandelbrot Z = Z * Z + c",
-			iterator: function(x, y, params) {
-				var Zx = 0.0;
-			    var Zy = 0.0;
+	mandelbrot_orbit_trap: {
+		title: "Mandelbrot / Orbit Trap",
+		desc: "A rendering of the Mandelbrot fractal using the Orbit trap algorithm (the trap being both Real and Imaginary axis)",
+		iterator: function(x, y) {
+			var Zx = 0.0;
+		    var Zy = 0.0;
 
-			    var converge = true;
-			    var xx = 0;
-			    var yy = 0;
-			    var mod = 0;
-			    var point_dist = 100000;
-			    var line_dist = 100000;
+		    var xx = 0;
+		    var yy = 0;
+		    var mod = 0;
+		    var point_dist = 100000;
+		    var line_dist = 100000;
 
-				for(var n = 0; n < MAX_ITERATIONS; n++) {
-			        //Z = Z * Z + c
-			        Zy = y + 2 * Zx * Zy;
-			        Zx = x + xx - yy;
+			for(var n = 0; n < MAX_ITERATIONS; n++) {
+		        //Z = Z * Z + c
+		        Zy = y + 2 * Zx * Zy;
+		        Zx = x + xx - yy;
 
-			        xx = Zx * Zx;
-			        yy = Zy * Zy;
+		        xx = Zx * Zx;
+		        yy = Zy * Zy;
 
-			        mod = xx * yy;
-			        mod2 = Math.min(xx, yy);
+		        mod = xx * yy;
+		        mod2 = Math.min(xx, yy);
 
-			        if( mod < point_dist ) {
-			        	point_dist = mod;
-			        }
+		        if( mod < point_dist ) {
+		        	point_dist = mod;
+		        }
 
-			        if( mod2 < line_dist ) {
-			        	line_dist = mod2;
-			        }
-			    }
+		        if( mod2 < line_dist ) {
+		        	line_dist = mod2;
+		        }
+		    }
 
-			    return pass_data({point_dist: Math.sqrt(point_dist), line_dist: Math.sqrt(line_dist), mod: mod, n: n, degree: 2});
-			},
+		    return Math.sqrt(line_dist);
 		},
-		mandelbrot_cubic: {
-			desc: "Cubic Mandelbrot Z = Z * Z * Z + c",
-			iterator: function(x, y, params) {
-
-				var Zx = 0.0;
-			    var Zy = 0.0;
-
-			    var converge = true;
-			    var xx = Zx * Zx;
-			    var yy = Zy * Zy;
-			    var mod = xx + yy;
-
-				for(var n = 0; n < MAX_ITERATIONS; n++) {
-			        //Z = Z * Z * Z + c
-			        Zx = x + xx * Zx - 3 * Zx * yy;
-			        Zy = y + 3 * xx * Zy - yy * Zy;
-
-			        xx = Zx * Zx;
-			        yy = Zy * Zy;
-			        mod = xx + yy;
-
-			        if( mod > CONVERGENCE_RADIUS_SQ ) {
-			            return pass_data({mod: mod, n: n, degree: 3});
-			        }
-			    }
-
-			    return true;
-			},
-		},
-		julia: {
-			desc: "Julia Z = Z * Z + c",
-			iterator: function(x, y, params) {
-				var Zx = x;
-			    var Zy = y;
-
-			    var cx = params.cx;
-			    var cy = params.cy;
-
-			    var xx = Zx * Zx;
-			    var yy = Zy * Zy;
-			    var mod = xx + yy;
-
-			    for(var n = 0; n < MAX_ITERATIONS; n++) {
-			        //Z = Z * Z + c
-			        Zy = cy + 2 * Zx * Zy;
-			        Zx = cx + xx - yy;
-
-			        xx = Zx * Zx;
-			        yy = Zy * Zy;
-
-			        mod = xx + yy;
-
-			        if( mod > CONVERGENCE_RADIUS_SQ ) {
-			            return pass_data({mod: mod, n: n, degree: 2});
-			        }
-			    }
-
-			    return false;
-			},
-		}
 	},
+	
+	julia_orbit_trap: {
+		title: "Julia / Orbit Trap",
+		desc: "A rendering of the Julia fractal around the point (-0.98, 0.34) using the Orbit trap algorithm (the trap being both Real and Imaginary axis)",
+		iterator: function(x, y) {
+			var Zx = x;
+		    var Zy = y;
 
-	pixelShaders: {
-		escape_time: {
-			desc: "Escape Time",
-			shader: function() {
-				return get_data('n') / MAX_ITERATIONS;
-			}
+		    var cx = -0.98;
+		    var cy = 0.34;
+
+		    var xx = 0;
+		    var yy = 0;
+		    var mod = 0;
+
+		    var point_dist = 100000;
+		    var line_dist = 100000;
+		    
+		    for(var n = 0; n < MAX_ITERATIONS; n++) {
+		        //Z = Z * Z + c
+		        Zy = cy + 2 * Zx * Zy;
+		        Zx = cx + xx - yy;
+
+		        xx = Zx * Zx;
+		        yy = Zy * Zy;
+
+		        mod = xx * yy;
+		        mod2 = Math.min(xx, yy);
+
+		        if( mod < point_dist ) {
+		        	point_dist = mod;
+		        }
+
+		        if( mod2 < line_dist ) {
+		        	line_dist = mod2;
+		        }
+		    }
+
+		    return Math.sqrt(line_dist);
 		},
-
-		real_escape_time: {
-			desc: "Real Escape Time",
-			shader: function() {
-				var lnDivisor = 1.0 / get_data('degree');
-
-                return (get_data('n') + (Math.log(Math.log(CONVERGENCE_RADIUS)) - Math.log(Math.log(Math.sqrt(get_data('mod'))))) * lnDivisor) / MAX_ITERATIONS;
-			}
-		},
-
-		point_orbit_trap: {
-			desc: "Orbit trap point min(Z)",
-			shader: function() {
-                return get_data('line_dist');
-			}
-		}
 	}
 }
